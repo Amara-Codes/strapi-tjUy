493c5e304c0c7b0fa4bfad56603e770535a7fc3e
Roberto Barbagallo <developer@greenleafstudio.it>
2025-06-24 19:53:21 +0700

    cors fixes

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,4 +1,16 @@
 module.exports = [
+    'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*', // Allows all headers
+      origin: ['http://localhost:3000', 'https://amarapub-production.up.railway.app', 'https://amara.pub'], // Add your frontend origins here
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'], // Allowed HTTP methods
+      credentials: true, // If you need to send cookies or authorization headers with credentials
+    },
+  },
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
